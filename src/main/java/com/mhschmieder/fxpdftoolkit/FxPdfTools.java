dad848d66f864ab0cb27fc1497cde94b6794b87c
Mark Schmieder <markhschmieder@gmail.com>
2023-03-26 23:19:11 -0400

    Initial checkin of simple JavaFX wrapper for pdftoolkit code

---
+++ src/main/java/com/mhschmieder/fxpdftoolkit/FxPdfTools.java
@@ -0,0 +1,97 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2020, 2023 Mark Schmieder
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * This file is part of the FxPdfToolkit Library
+ *
+ * You should have received a copy of the MIT License along with the
+ * FxPdfToolkit Library. If not, see <https://opensource.org/licenses/MIT>.
+ *
+ * Project: https://github.com/mhschmieder/fxpdftoolkit
+ */
+package com.mhschmieder.fxpdftoolkit;
+
+import java.awt.image.BufferedImage;
+
+import com.mhschmieder.fxgraphicstoolkit.image.ImageUtilities;
+import com.mhschmieder.pdftoolkit.PdfFonts;
+import com.mhschmieder.pdftoolkit.PdfTools;
+import com.pdfjet.Image;
+import com.pdfjet.PDF;
+import com.pdfjet.Page;
+
+import javafx.scene.Node;
+import javafx.scene.layout.Region;
+
+public class FxPdfTools {
+
+    public FxPdfTools() {
+        // NOTE Auto-generated constructor stub
+    }
+
+    public static Image getImageSnapshot( final PDF document, final Node node ) {
+        // Get an AWT BufferedImage as a snapshot of the source node.
+        final BufferedImage bufferedImage = ImageUtilities.getBufferedImageSnapshot( node );
+
+        // Convert to a PdfJet Image.
+        final Image image = PdfTools.getImageSnapshot( document, bufferedImage );
+
+        return image;
+    }
+
+    public static double writeVisualization( final PDF document,
+                                             final Page visualizationPage,
+                                             final PdfFonts fonts,
+                                             final String chartLabel,
+                                             final double layoutWidth,
+                                             final boolean useChart1,
+                                             final Region chart1,
+                                             final boolean useChart2,
+                                             final Region chart2,
+                                             final boolean useLegend,
+                                             final Region chartLegend )
+            throws Exception {
+        // Get an AWT BufferedImage as a snapshot of the first chart.
+        final BufferedImage chart1Snapshot = ImageUtilities.getBufferedImageSnapshot( chart1 );
+
+        // Get an AWT BufferedImage as a snapshot of the second chart.
+        final BufferedImage chart2Snapshot = ImageUtilities.getBufferedImageSnapshot( chart2 );
+
+        // Get an AWT BufferedImage as a snapshot of the chart legend.
+        final BufferedImage chartLegendSnapshot = ImageUtilities
+                .getBufferedImageSnapshot( chartLegend );
+
+        final double metadataAdjustmentY = PdfTools.writeVisualization( document,
+                                                                        visualizationPage,
+                                                                        fonts,
+                                                                        chartLabel,
+                                                                        layoutWidth,
+                                                                        useChart1,
+                                                                        chart1Snapshot,
+                                                                        useChart2,
+                                                                        chart2Snapshot,
+                                                                        useLegend,
+                                                                        chartLegendSnapshot );
+
+        return metadataAdjustmentY;
+    }
+}
